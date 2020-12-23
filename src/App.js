f4cdb8687b1ef9d0b39a6f5828d290cf2ddc5274
Brandon Austin <61210295+branaus@users.noreply.github.com>
2020-12-23 10:05:21 -0800

    mutating state the safe way

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import ScoreKeeper from './ScoreKeeper'
+import IconList from './IconList'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ScoreKeeper />
+      <IconList />
     </div>
   );
 }
